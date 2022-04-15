1c8fad0b0d969b18ad58a1975c6833bfb8c7dee7
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2022-04-15 20:53:38 +0100

    extended django admin

--- blog_app/user_auth/admin.py
+++ blog_app/user_auth/admin.py
@@ -5,5 +5,5 @@ from blog_app.user_auth.models import BlogUser
 
 @admin.register(BlogUser)
 class BlogUserAdmin(admin.ModelAdmin):
-    list_display = ['username']
+    list_display = ['username', 'is_staff', 'is_superuser']
     filter_horizontal = ('groups', 'user_permissions')
